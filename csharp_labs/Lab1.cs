d8076a396596601f71ca97bdb2eca36220060e4d
OstapKlymochko <klymochkoo@gmail.com>
2023-02-20 18:55:17 +0200

    Add project files.

---
+++ csharp_labs/Lab1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab1// Note: actual namespace depends on the project name.
+{
+    internal class Lab1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
